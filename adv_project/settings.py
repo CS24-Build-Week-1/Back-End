bd46e2e211998d04b3c25b95a8dc3659674d96c5
LaVon Mejia <lavonmejia@gmail.com>
2020-01-07 11:28:09 -0500

    getting started

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+# SECRET_KEY = 'MESSY'
+# DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
-
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
