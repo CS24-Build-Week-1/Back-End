edd63302f3422f5e0a880a684319c6d44995f455
ren.meta@gmail.com <ren.meta@gmail.com>
2020-01-10 19:55:15 -0500

    add Item to django admin interface

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Room, Player
+from .models import Room, Player, Item
 # from .world_generate import World
 
 class RoomInline(admin.TabularInline):
@@ -9,13 +9,19 @@ class RoomInline(admin.TabularInline):
 class PlayerInline(admin.TabularInline):
     model = Player
 
+class ItemInline(admin.TabularInline):
+    model = Item
+
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'n_to','s_to','e_to','w_to','pos_x', 'pos_y')
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('user','currentRoom','uuid')
+    list_display = ('user','currentRoom','uuid', 'inventory')
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'location')
 
 
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Item, ItemAdmin)
